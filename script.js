a937fc0148baa2b3fd58a137aeb224ff8348ac02
quentinburke96 <qeburke.qe@gmail.com>
2022-06-18 22:33:51 -0400

    Getting contact form to run

---
+++ script.js
@@ -0,0 +1,12 @@
+// Get data
+const nameInput = document.querySelector("#name");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const success = document.querySelector("#success");
+const errorNodes = document.querySelectorAll(".error");
+
+// Validate data
+function validateForm(){
+    console.log(fiwrufhwihwhuhiwrufhirwufh);
+
+}
